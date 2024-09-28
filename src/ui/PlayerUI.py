a70fd841e837d86f4aa42e7f106852b3ee43c7bd
alessandrogia <alexgiacento@gmail.com>
2024-09-28 14:14:49 +0200

    playerui for play previous fixed

--- src/ui/PlayerUI.py
+++ src/ui/PlayerUI.py
@@ -47,10 +47,14 @@ class BackButton(Button):
     async def callback(self, interaction):
         if self.__voice_state.position(interaction) // 1000 > 5 or self.__voice_state.queue_history(interaction).count < 2:
             await self.__voice_state.restart(interaction)
+            view = self.view
         else:
             await self.__voice_state.play_previous(interaction)
+            view = None
 
-        await interaction.response.edit_message(view=self.view)
+        await interaction.response.edit_message(view=view)
+
+
 
 class ShuffleButton(Button):
     def __init__(self, voice_state, guild_id: int, row: int):
